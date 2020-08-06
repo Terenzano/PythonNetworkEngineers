70e6191204af4591b906ca48b55f7dfa5b3a9eec
Terenzano <naikatepa@gmail.com>
2020-08-06 17:07:59 +0300

    added napalmv3.py

---
+++ napalmv3.py
@@ -0,0 +1,12 @@
+import json
+from napalm import get_network_driver
+driver = get_network_driver('ios')
+iosvl2 = driver('192.168.122.72', 'cisco', 'cisco')
+iosvl2.open()
+
+ios_output = iosvl2.ping('google.com')
+print (json.dumps(ios_output, sort_keys=True, indent=4))
+
+ios_output = iosvl2.traceroute('google.com')
+print (json.dumps(ios_output, sort_keys=True, indent=4))
+
