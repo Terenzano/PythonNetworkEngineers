a05cf00eab8bc649cb56109fcf48b734364499c2
Terenzano <naikatepa@gmail.com>
2020-08-06 13:22:02 +0300

    modified napalmv1.py with json

--- napalmv1.py
+++ napalmv1.py
@@ -1,8 +1,9 @@
+import json
 from napalm import get_network_driver
 driver = get_network_driver('ios')
 iosvl2 = driver('192.168.122.72', 'cisco', 'cisco')
 iosvl2.open()
 
 ios_output = iosvl2.get_facts()
-print (ios_output)
+print (json.dumps(ios_output, indent=4))
 
